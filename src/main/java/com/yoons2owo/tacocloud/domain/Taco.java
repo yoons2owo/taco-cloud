8eebf171e7f3a292f8d33f84cc628bb3b5ea8bad
yoon <yoons2owo@gmail.com>
2021-04-03 22:31:01 +0900

    feat: add domain (taco, ingredient)

---
+++ src/main/java/com/yoons2owo/tacocloud/domain/Taco.java
@@ -0,0 +1,13 @@
+package com.yoons2owo.tacocloud.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<String> ingredients;
+
+}
